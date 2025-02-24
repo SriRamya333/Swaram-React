cec4af949cfeb892f97b9ebf212c21ff3da793db
SriRamya333 <msriramya333@gmail.com>
2025-02-24 13:18:46 +0530

    ramya

--- src/components/why.tsx
+++ src/components/why.tsx
@@ -1,52 +1,56 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Why = () => {
-    return (
-        <>
-          <div className="container">
-            <div className="row">
-                <div className="content col-lg-12 col-md-12 mx-auto col-sm-12">
-               <h4>why choose us</h4>
-                </div>
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="content col-lg-12 col-md-12 mx-auto col-sm-12">
+            <h4>why choose us</h4>
+            <div className="col">
+              <div className="border border-success border-1 opacity-100 w-25"></div>
             </div>
-            <div className="row mt-3">
-                <div className="decade col-lg-6 col-md-6 col-12">
-                 <h2>Decades of experience, global reach, and a commitment to <span className='why-content'>excellence</span></h2>
-                </div>
-            </div>
-            <div className="row mt-4">
-                <div className="col-lg-4 col-md-4 col-12">
-                    <div className='global' >
-                        <img src="/global.png" className="why-icon"/>
-                    </div>
-                    <div className="icon-content mt-2">
-                        <h6>Global Expertise</h6>
-                        <p>Decades of experience across multiple sectors, providing tailored services</p>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-4 col-12">
-                <div className='global1'>
-                        <img src="/setting.png" className="why-icon1"/>
-                    </div>
-                    <div className="icon-content mt-2">
-                        <h6>End-to-End Solutions</h6>
-                        <p>Integrated services, from sourcing to full-scale development, streamlining operations and enhancing efficiency.
-                        </p>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-4 col-12">
-                <div className='global' >
-                        <img src="/bulb.png" className="why-icon2"/>
-                    </div>
-                    <div className="icon-content mt-2">
-                        <h6>Global Expertise</h6>
-                        <p>Adoption of the latest technologies to offer cutting-edge solutions.
-                        </p>
-                    </div>
-                </div>
-            </div>
+          </div>
+
         </div>
-        {/* <div className="container py-5">
+        <div className="row mt-3">
+          <div className="decade col-lg-6 col-md-6 col-12">
+            <h2>Decades of experience, global reach, and a commitment to <span className='why-content'>excellence</span></h2>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col-lg-4 col-md-4 col-12">
+            <div className='global' >
+              <img src="/global.png" className="why-icon" />
+            </div>
+            <div className="icon-content mt-2">
+              <h6>Global Expertise</h6>
+              <p>Decades of experience across multiple sectors, providing tailored services</p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-12">
+            <div className='global1'>
+              <img src="/setting.png" className="why-icon1" />
+            </div>
+            <div className="icon-content mt-2">
+              <h6>End-to-End Solutions</h6>
+              <p>Integrated services, from sourcing to full-scale development, streamlining operations and enhancing efficiency.
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-4 col-12">
+            <div className='global' >
+              <img src="/bulb.png" className="why-icon2" />
+            </div>
+            <div className="icon-content mt-2">
+              <h6>Global Expertise</h6>
+              <p>Adoption of the latest technologies to offer cutting-edge solutions.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="container py-5">
    
           <div className="row align-items-center">
             <div className="col-auto">
@@ -104,9 +108,9 @@ const Why = () => {
             </div>
           </div>
         </div> */}
-      </>
-      
-      )
+    </>
+
+  )
 
 }
 
