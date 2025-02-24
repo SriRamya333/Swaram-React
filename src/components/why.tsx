17ab421f9a197757d56affd5b1193c58cca93d24
SriRamya333 <msriramya333@gmail.com>
2025-02-24 13:16:52 +0530

    ramya

--- src/components/why.tsx
+++ src/components/why.tsx
@@ -3,8 +3,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Why = () => {
     return (
         <>
-        <div className="container py-5">
-          {/* Section Header */}
+          <div className="container">
+            <div className="row">
+                <div className="content col-lg-12 col-md-12 mx-auto col-sm-12">
+               <h4>why choose us</h4>
+                </div>
+            </div>
+            <div className="row mt-3">
+                <div className="decade col-lg-6 col-md-6 col-12">
+                 <h2>Decades of experience, global reach, and a commitment to <span className='why-content'>excellence</span></h2>
+                </div>
+            </div>
+            <div className="row mt-4">
+                <div className="col-lg-4 col-md-4 col-12">
+                    <div className='global' >
+                        <img src="/global.png" className="why-icon"/>
+                    </div>
+                    <div className="icon-content mt-2">
+                        <h6>Global Expertise</h6>
+                        <p>Decades of experience across multiple sectors, providing tailored services</p>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-4 col-12">
+                <div className='global1'>
+                        <img src="/setting.png" className="why-icon1"/>
+                    </div>
+                    <div className="icon-content mt-2">
+                        <h6>End-to-End Solutions</h6>
+                        <p>Integrated services, from sourcing to full-scale development, streamlining operations and enhancing efficiency.
+                        </p>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-md-4 col-12">
+                <div className='global' >
+                        <img src="/bulb.png" className="why-icon2"/>
+                    </div>
+                    <div className="icon-content mt-2">
+                        <h6>Global Expertise</h6>
+                        <p>Adoption of the latest technologies to offer cutting-edge solutions.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {/* <div className="container py-5">
+   
           <div className="row align-items-center">
             <div className="col-auto">
               <span className="bg-success bg-opacity-25 text-success fw-bold px-3 py-2 rounded">
@@ -16,7 +59,7 @@ const Why = () => {
             </div>
           </div>
       
-          {/* Title */}
+
           <div className="row mt-3">
             <div className="col-12 text-center text-md-start">
               <h2>
@@ -26,9 +69,9 @@ const Why = () => {
             </div>
           </div>
       
-          {/* Features Section */}
+   
           <div className="row mt-4 row-cols-1 row-cols-md-3 g-4">
-            {/* Global Expertise */}
+    
             <div className="col text-center">
               <div className="d-flex justify-content-center">
                 <img src="/global.png" className="img-fluid" style={{ width: "60px", height: "60px" }} />
@@ -39,7 +82,6 @@ const Why = () => {
               </div>
             </div>
       
-            {/* End-to-End Solutions */}
             <div className="col text-center">
               <div className="d-flex justify-content-center">
                 <img src="/setting.png" className="img-fluid" style={{ width: "60px", height: "60px" }} />
@@ -50,7 +92,7 @@ const Why = () => {
               </div>
             </div>
       
-            {/* Innovation & Technology */}
+        
             <div className="col text-center">
               <div className="d-flex justify-content-center">
                 <img src="/bulb.png" className="img-fluid" style={{ width: "60px", height: "60px" }} />
@@ -61,7 +103,7 @@ const Why = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </>
       
       )
