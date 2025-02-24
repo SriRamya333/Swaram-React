6615cb16303f2fb7a12ffe77c65b7f7065d70321
SriRamya333 <msriramya333@gmail.com>
2025-02-24 12:28:49 +0530

    ramya

--- src/components/future.tsx
+++ src/components/future.tsx
@@ -2,28 +2,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Future = () => {
     return (
-      <div className="container text-center">
-      {/* Heading */}
-      <div className="popular mb-4">
-        <h2>We Featured on Popular Partners Like</h2>
+      <div className="container">
+      <div className="popular">
+      <h2>We Featured on Popular Partners Like</h2>
       </div>
-    
-      {/* Logo Grid */}
-      <div className="row justify-content-center g-3 mt-5">
-        <div className="partner col-6 col-md-3 d-flex justify-content-center">
-          <img src="/Fortune.png" className="img-fluid" alt="Fortune" style={{ maxWidth: "150px", height: "auto" }} />
+       <div className="row text-center mt-5">
+        <div className="partner col-lg-3 col-md-3 col-12">
+           <img src="/Fortune.png"/>
         </div>
-        <div className="col-6 col-md-3 d-flex justify-content-center">
-          <img src="/Forbes.png" className="img-fluid" alt="Forbes" style={{ maxWidth: "150px", height: "auto" }} />
+        <div className="col-lg-3  col-md-3 col-12">
+          <img src="/Forbes.png"/>
         </div>
-        <div className="col-6 col-md-3 d-flex justify-content-center">
-          <img src="/invest-detroit.png" className="img-fluid" alt="Invest Detroit" style={{ maxWidth: "150px", height: "auto" }} />
+        <div className="col-lg-3 col-md-3 col-12">
+        <img src="/invest-detroit.png"/>
         </div>
-        <div className="col-6 col-md-3 d-flex justify-content-center">
-          <img src="/Robinhood.png" className="img-fluid" alt="Robinhood" style={{ maxWidth: "150px", height: "auto" }} />
+        <div className="col-3 col-md-3 col-12">
+        <img src="/Robinhood.png"/>
         </div>
-      </div>
+       </div>
     </div>
+
+
+    //   <div className="container text-center">
+      
+    //   <div className="popular mb-4">
+    //     <h2>We Featured on Popular Partners Like</h2>
+    //   </div>
+    
+      
+    //   <div className="row justify-content-center g-3 mt-5">
+    //     <div className="partner col-6 col-md-3 d-flex justify-content-center">
+    //       <img src="/Fortune.png" className="img-fluid" alt="Fortune" style={{ maxWidth: "150px", height: "auto" }} />
+    //     </div>
+    //     <div className="col-6 col-md-3 d-flex justify-content-center">
+    //       <img src="/Forbes.png" className="img-fluid" alt="Forbes" style={{ maxWidth: "150px", height: "auto" }} />
+    //     </div>
+    //     <div className="col-6 col-md-3 d-flex justify-content-center">
+    //       <img src="/invest-detroit.png" className="img-fluid" alt="Invest Detroit" style={{ maxWidth: "150px", height: "auto" }} />
+    //     </div>
+    //     <div className="col-6 col-md-3 d-flex justify-content-center">
+    //       <img src="/Robinhood.png" className="img-fluid" alt="Robinhood" style={{ maxWidth: "150px", height: "auto" }} />
+    //     </div>
+    //   </div>
+    // </div>
     
       )
 
