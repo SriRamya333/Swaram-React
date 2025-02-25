d3353dbdccaa4726568a27a0c4c978674e4c9b72
SriRamya333 <msriramya333@gmail.com>
2025-02-25 11:41:00 +0530

    ramya

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import About from "./about";
+// import About from "./about";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
